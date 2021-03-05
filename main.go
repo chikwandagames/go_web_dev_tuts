ce273c59f2d3c1765d2590c85f4e4e1f11a8afed
chikwandagames <chikwandagames@gmail.com>
2021-03-05 21:56:15 +0000

    inserting data into HTML template

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+//  {{/* a comment */}}	Defines a comment
+/*
+{{.}}	Renders the root element
+{{.Name}}	Renders the “Name”-field in a nested element
+{{if .Done}} {{else}} {{end}}	Defines an if/else-Statement
+{{range .List}} {{.}} {{end}}	Loops over all “List” field and renders each using {{.}}
+*/
+
+var tpl *template.Template
+var name = "John"
+
+func main() {
+	tpl, _ = tpl.ParseGlob("templates/*.html")
+	http.HandleFunc("/welcome", welcomeHandler)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("IndexHandler running")
+	tpl.ExecuteTemplate(w, "welcome.html", name)
+}
