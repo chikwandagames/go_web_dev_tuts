7a018687c0020f94d71dbb3269607f84665e1806
chikwandagames <chikwandagames@gmail.com>
2021-03-05 18:07:04 +0000

    parsing files in dirs

---
+++ temp2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func main() {
+
+	// func ParseFiles(filenames ...string) (*Template, error)
+	// tpl, _ = template.ParseFiles("index1.html")
+	// tpl, _ = template.ParseFiles("data1/index2.html")
+	// tpl, _ = template.ParseFiles("data1/data2/index3.html")
+	tpl, _ = template.ParseFiles("../index4.html")
+
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
