9710272e107db46bcb6ec0211b4512a5b6dc277b
chikwandagames <chikwandagames@gmail.com>
2021-03-05 18:14:03 +0000

    parsing alternative

--- temp2/main.go
+++ temp2/main.go
@@ -13,7 +13,10 @@ func main() {
 	// tpl, _ = template.ParseFiles("index1.html")
 	// tpl, _ = template.ParseFiles("data1/index2.html")
 	// tpl, _ = template.ParseFiles("data1/data2/index3.html")
-	tpl, _ = template.ParseFiles("../index4.html")
+	// tpl, _ = template.ParseFiles("../index4.html")
+
+	// Alternatively, we could use the ParseFiles directly
+	tpl, _ = tpl.ParseFiles("../index4.html")
 
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8080", nil)
